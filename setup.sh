dbe2d5dba4bb9f18b96e46dbe0d12ef61ca138ad
v15hv4 <vishva2912@gmail.com>
2024-06-20 02:33:36 +0530

    More stuff

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 REPO_URL=https://github.com/v15hv4/init
 
 # directory to clone this repo into
-INIT_DIR=$HOME/dotfiles
+INIT_DIR=$HOME/init
 
 # packages to install
 PACKAGES=(
@@ -14,6 +14,7 @@ PACKAGES=(
   feh
   zsh
   bat
+  dex
   eza
   tmux
   htop
@@ -21,16 +22,19 @@ PACKAGES=(
   make
   wget
   rsync
+  kitty
   unzip
+  picom
   upower
   polkit
   ripgrep
+  lightdm
+  ntfs-3g
   os-prober
   playerctl
   flameshot
   efibootmgr
   polkit-gnome
-  optimus-manager
 
   # de/wm
   rofi
@@ -40,12 +44,8 @@ PACKAGES=(
   arandr
   polybar
   autorandr
+  xorg-xsetroot
   lxappearance-gtk3
-  qt5-svg
-  qt5-base
-  qt5-x11extras
-  qt5-declarative
-  qt5-translations
 
   # audio
   alsa-lib                                                                                                 
@@ -60,7 +60,6 @@ PACKAGES=(
   pipewire
   pipewire-alsa
   pipewire-audio
-  pipewire-jack
   pipewire-pulse
   wireplumber
 
@@ -76,9 +75,9 @@ PACKAGES=(
 
   # fonts
   noto-fonts
-  # apple-fonts
   ttf-opensans
   noto-fonts-cjk
+  otf-apple-fonts
   noto-fonts-emoji
   noto-fonts-extra
   otf-geist-mono-nerd
@@ -97,6 +96,10 @@ PACKAGES=(
   telegram-desktop-bin
 )
 
+# install yay
+echo "installing yay..."
+sudo pacman -Syu yay --noconfirm
+
 # update yay
 echo "updating yay..."
 yay -Syu --noconfirm
@@ -108,7 +111,7 @@ yay -S ${PACKAGES[@]} --noconfirm
 # zsh plugins
 echo "installing zsh plugins..."
 ## oh-my-zsh
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 ## zsh-autosuggestions
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 ## powerlevel10k
@@ -118,24 +121,40 @@ git clone --depth=1 https://github.com/zpm-zsh/autoenv ${ZSH_CUSTOM:-$HOME/.oh-m
 
 # clone repo
 echo "cloning repo..."
-git clone $REPO_URL $INIT_DIR
+[ ! -e $INIT_DIR ] && git clone $REPO_URL $INIT_DIR
 
 # link .configs
 for filename in $(ls -A $INIT_DIR/.config); do
   [ -e ~/.config/$filename ] && mv ~/.config/$filename ~/.config/$filename.old
-  ln -s $(realpath $filename) ~/.config
+  ln -s $(realpath $INIT_DIR/.config/$filename) ~/.config
 done
 
 # link dotfiles
 for filename in $(ls -A $INIT_DIR | grep -v -E ".git|.config|setup.sh"); do
   [ -e ~/$filename ] && mv ~/$filename ~/$filename.old
-  ln -s $(realpath $filename) ~
+  ln -s $(realpath $INIT_DIR/$filename) ~
 done
 
 # stuff and things
 ln -s /tmp ~/tmp
 sudo mkdir /mnt/ext
+sudo systemctl enable bluetooth
+
+# nvidia
+yay -S optimus-manager --noconfirm
+sudo sed -i "s/^\(startup_mode=\).*/\1auto/g" /etc/optimus-manager/optimus-manager.conf # autodetect graphics on startup
 
 # setup git
+echo "setting up git..."
 git config --global user.name "v15hv4"
 git config --global user.email "vishva2912@gmail.com"
+
+# set up container runtime
+yay -Syu podman podman-compose podman-docker --noconfirm
+sudo touch /etc/containers/nodocker
+
+# clean up yay cache
+yay -R $(yay -Qtdq) --noconfirm
+yay -Scc --noconfirm
+
+echo "done!"
