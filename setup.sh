aa6abfb794c7d93900a833e078f1a9bdf64a282a
v15hv4 <vishva2912@gmail.com>
2025-01-09 15:05:12 +0530

    Update work setup.sh

--- setup.sh
+++ setup.sh
@@ -20,7 +20,6 @@ PACKAGES=(
   feh
   zsh
   bat
-  dex
   eza
   tmux
   htop
@@ -31,52 +30,17 @@ PACKAGES=(
   kitty
   unzip
   picom
-  upower
-  polkit
   ripgrep
   ntfs-3g
   os-prober
-  playerctl
   flameshot
   base-devel
   efibootmgr
-  # polkit-gnome
-  # brightnessctl
-  # inotify-tools
 
   # de/wm
-  # rofi
-  # bspwm
-  # dunst
-  # sxhkd
-  # arandr
-  # polybar
-  # autorandr
-  # i3lock-color
-  # xorg-xsetroot
-  # lxappearance-gtk3
-
-  # audio
-  # alsa-lib                                                                                                 
-  # alsa-utils
-  # alsa-plugins
-  # alsa-ucm-conf
-  # alsa-firmware
-  # alsa-card-profiles
-  # alsa-topology-conf
-  # pamixer
-  # pavucontrol
-  # pipewire
-  # pipewire-alsa
-  # pipewire-audio
-  # pipewire-pulse
-  # wireplumber
-
-  # bluetooth
-  # bluez
-  # bluez-libs
-  # bluez-utils
-  # blueman
+  bspwm
+  sxhkd
+  autorandr
 
   # editor
   neovim
@@ -102,17 +66,12 @@ PACKAGES=(
   google-chrome
   zathura
   zathura-pdf-poppler
-  slack-desktop
   ansible
   magic-wormhole
 
-
-  # # virtualization
-  # podman
-  # podman-compose
-  # podman-docker
-  # vagrant libvirt
-  # qemu-full 
+  # virtualization
+  libvirt
+  qemu-full 
 
   # snapshots
   grub-btrfs
@@ -140,18 +99,6 @@ uninstall_packages() {
   yay -R alacritty kvantum cachy-browser cachyos-hello btop eog fish cachyos-gnome-settings fisher fish-autopair fish-pure-prompt cachyos-fish-config kvantum-theme-libadwaita-git --noconfirm
 }
 
-install_zsh_plugins() {
-  echo "installing zsh plugins..."
-  # oh-my-zsh
-  sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-  # zsh-autosuggestions
-  git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-  # powerlevel10k
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-  # autoenv
-  git clone --depth=1 https://github.com/zpm-zsh/autoenv ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/autoenv
-}
-
 setup_dotfiles() {
   # link .configs
   if [ -e $1/.config ]; then
@@ -172,13 +119,6 @@ setup_user() {
   sudo usermod -aG video $(whoami)
 }
 
-setup_gtk() {
-  gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita'
-  gsettings set org.gnome.desktop.interface icon-theme 'Adwaita' 
-  gsettings set org.gnome.desktop.interface cursor-theme 'capitaine-cursors'
-  gsettings set org.gnome.desktop.interface font-name 'SF Pro Text 10'
-}
-
 setup_filesystem() {
   ln -s /tmp ~/tmp
   sudo mkdir /mnt/ext
@@ -190,16 +130,6 @@ setup_systemctl() {
   sudo systemctl enable nfs-server
   sudo systemctl enable grub-btrfsd
 }
-
-# setup_podman() {
-#   sudo touch /etc/containers/nodocker
-#   echo 'unqualified-search-registries = ["docker.io"]' | sudo tee -a /etc/containers/registries.conf
-# }
-
-# setup_vagrant() {
-#   export VAGRANT_DISABLE_STRICT_DEPENDENCY_ENFORCEMENT=1
-#   vagrant plugin install vagrant-libvirt
-# }
 
 setup_snapshots() {
   sed "s:\(grub-btrfsd --syslog\) /.snapshots:\1 -t:g" /etc/systemd/system/grub-btrfsd.service
@@ -214,14 +144,10 @@ cleanup() {
 main() {
   install_yay
   install_packages
-  install_zsh_plugins
 
   setup_user
-  setup_gtk
   setup_filesystem
   setup_systemctl
-  # setup_podman
-  # setup_vagrant
 
   [ -e $DOTFILES_DIR ] && rm -rf $DOTFILES_DIR.old && mv $DOTFILES_DIR $DOTFILES_DIR.old
   git clone $REPO_URL -b $DOTFILES_BRANCH $DOTFILES_DIR
